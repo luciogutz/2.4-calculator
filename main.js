fde9b6509f62dce5bd850de61bcbe80647c4376f
Lucio Gutierrez <lucio.gutierrez30@yahoo.com>
2016-12-08 16:09:44 -0600

    changed textarea section into <h2> for better success in javascript

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 document.addEventListener("DOMContentLoader", function(){
 
-    var button_7_Element = document.querySelector("[data-js'button__7']")
+    var button_7_Element = document.querySelector("[data-js'button__7']");
 
     button_7_Element.addEventListener("click"; function(){
-      
+
+
     })
 
 })
