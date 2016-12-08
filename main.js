2eef3512b30f7aef595027737ae30dad0441e95d
Lucio Gutierrez <lucio.gutierrez30@yahoo.com>
2016-12-08 15:43:34 -0600

    started main.js, started button 7 code

---
+++ main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoader", function(){
+
+    var button_7_Element = document.querySelector("[data-js'button__7']")
+
+    button_7_Element.addEventListener("click"; function(){
+      
+    })
+
+})
